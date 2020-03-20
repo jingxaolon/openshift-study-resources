34cc6fd7f6df806cf321af38bb0018cd6a09a10d
jingxaolon <1198285059@qq.com>
2020-03-20 15:48:02 +0800

    add script

---
+++ openshift/build_openshift_v1.3.0.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#######################################################################
+# 1、VPS环境(澳洲)
+# 2、Centos 7
+#######################################################################
+
+#hostname set
+#略
+
+#install docker
+yum install -y docker vim net-tools
+systemctl enable docker
+systemctl restart docker
+systemctl disable firewalld
+systemctl stop firewalld
+
+#install GUI
+yum groupinstall -y "GNOME Desktop" "Graphical Administration Tools"
+
+#get binary
+cd /opt
+wget http://github.com/openshift/origin/releases/download/v1.3.0/openshift-origin-server-v1.3.0-3ab7af3d097b57f933eccef684a714f2368804e7-linux-64bit.tar.gz
+sleep 1
+tar -zxf openshift-origin-server-v1.3.0-3ab7af3d097b57f933eccef684a714f2368804e7-linux-64bit.tar.gz
+ln -s openshift-origin-server-v1.3.0-3ab7af3d097b57f933eccef684a714f2368804e7-linux-64bit /opt/openshift
+echo "PATH=$PATH:/opt/openshift" >> /etc/profile
+. /etc/profile
+openshift version
+sleep 1
+
+#set runlevel 5
+systemctl set-default graphical
